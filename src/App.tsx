e64a81cbf6ed2844948ddaf63e92203e6f010a12
David Campbell III <lapidistcubed@gmail.com>
2025-07-15 23:34:15 -0500

    First

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import NavBar from './components/NavBar';
+
+export default function App() {
+
+  return (
+    <div className='bg-gray-900 h-full w-full min-h-[100vh] min-w-full'>
+      <div className='max-w-[1600px] mx-auto'>
+        <NavBar />
+      </div>
+    </div>
+  )
+}
